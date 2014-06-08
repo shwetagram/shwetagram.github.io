2bd44ca43c2b67ae36366deea7f07d6d73e586e7
Grampurohit <shwetag@14109fddb741.ant.amazon.com>
2014-06-08 15:25:24 -0700

    cleaned up ui

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
+
+
 $(document).ready(function() {
-//makes list sortable
+
+//Makes list sortable
 	$('ol').sortable();
-    //making enter button work
+
+
+    //Enter button
 	$("input").keypress(function(event) {
     if (event.which == 13) {
         event.preventDefault();
         var toAdd = $('input[name=checkListItem]').val();
-        $('.list').append('<li class="item">' + toAdd + '</li>');
+        $('.list').prepend('<li class="item">' + toAdd + '</li>');
+        
+//Empty text field after hitting Enter
+        $('input').val('');
     };
 });	
+
+//Removing list items on click
+
 $(document).on('click', '.item', function() {
         $(this).remove();
+        $("input").text("removed");
     });
-$('ol').hover(function(){
-    $(this).append("<div>'.close'</div>");
-
-});
-
-/*
-    //this isn't working
-$("input").focus(function(){
-    $("#flip form").addClass("ready");
-});
-    $(document).on('click', '.item', function() {
-        $(this).remove();
-    });
-    $("input").blur(function(){
-    if($(this).val() == "")
-        $("#flip form").removeClass("ready");
-});
-    //end of non working code
-*/
-
-$("#todo").focus(function(){
-    $("#flip form").addClass("ready");
-})
-
-
-
 
 });
